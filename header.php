39bdb11814a04550e33ef6bb629bde782a2d2047
Andreas Papas <rsouflaki@yahoo.com>
2011-10-23 18:48:21 +0200

    improve header

--- header.php
+++ header.php
@@ -4,9 +4,21 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <title>To Do Site</title>
+        <link rel="stylesheet" type="text/css" href="menuformating.css" />
         <script type="text/javascript" src="functions.js"></script>
     </head> 
     <body>
-    
+        <div id="pagestyle">
+            <div id="menubar">
+                <div id="menulogo">
+                    <h1>
+                        <a href="login.php">
+                            <img src="images/logo.jpg" alt="Company Logo" border="0" align="top" width="48" height="48" />
+                        </a>
+                        PHP Exercise
+                    </h1>
+                </div>
+            </div>
+
     
     
